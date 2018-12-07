5dd920b75a686d3a68c3cce33dae09fc66dc5816
danielk6 <danielk6@vt.edu>
2018-12-07 15:59:40 -0500

    Knights and Minimax

--- AlphaBeta_Pruning_Chess_Engine.py
+++ AlphaBeta_Pruning_Chess_Engine.py
@@ -35,22 +35,83 @@ def move(board, fromMove, toMove):
     board[8 - int(toMove[1])][toCol] = piece
     return board
 
+pawnScore = [[0, 0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, -4, -4, 0, 0, 0],
+            [2, 2, 4, 6, 6, 4, 2, 2],
+            [4, 6, 8, 16, 16, 8, 6, 4],
+            [8, 10, 12, 18, 18, 12, 10, 8],
+            [12, 14, 16, 21, 21, 16, 14, 12],
+            [20, 26, 26, 28, 28, 26, 26, 20],
+            [0, 0, 0, 0, 0, 0, 0, 0]]
+
+knightScore =   [[-50, -20, -10, -10, -10, -10, -20, -50], 
+                [-5, 0, 5, 5, 5, 5, 0, -5], 
+                [-5, 5, 8, 8, 8, 8, 5, -5], 
+                [-5, 5, 10, 15, 15, 10, 5, -5], 
+                [-5, 5, 10, 15, 15, 10, 5, -5], 
+                [-5, 5, 10, 15, 15, 10, 5, -5], 
+                [-5, 5, 5, 5, 5, 5, 5, -5],
+                [-40, -10, -5, -5, -5, -5, -10, -40]] 
+
 
 def scoreBoard(board):
-    return 0
+    score = 0
+    for row in range(0,8):
+        for col in range(0,8):
+            if board[row][col] in "p":
+                score = score + pawnScore[row][col]
+            elif board[row][col] in "n":
+                score = score + knightScore[row][col]
+            elif board[row][col] in "P":
+                score = score - pawnScore[7 - row][col]
+            elif board[row][col] in "N":
+                score = score - knightScore[7 - row][col]
+    return score
 
-def possibleMoves(board, row, col):
+def possibleMoves(board, row, col, moves):
     cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     copyBoard = copy.deepcopy(board)
-    moves = []
-    if copyBoard[row][col] == 'p':
+    if copyBoard[row][col] == 'a':
         if copyBoard[row + 1][col] == '.' :
             fromPos = cols[col] + str(8 - row)
             toPos = cols[col] + str(7 - row)
             moves.append(move(copy.deepcopy(board), fromPos, toPos))
-            if row == 1 and copyBoard[row + 1][col] == '.': 
+            if row == 1 and copyBoard[row + 2][col] == '.': 
                 toPos = cols[col] + str(6 - row)
                 moves.append(move(copy.deepcopy(board), fromPos, toPos))
+    if copyBoard[row][col] == 'n':
+        if row + 1 < 8 and col + 2 < 8 and copyBoard[row + 1][col + 2] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col + 2] + str(7 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos))  
+        if row + 1 < 8 and col - 2 >= 0 and copyBoard[row + 1][col - 2] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col - 2] + str(7 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos)) 
+        if row - 1 >= 0 and col - 2 >= 0 and copyBoard[row - 1][col - 2] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col - 2] + str(9 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos)) 
+        if row - 1 >= 0 and col + 2 < 8 and copyBoard[row - 1][col + 2] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col + 2] + str(9 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos))
+        if row + 2 < 8 and col + 1 < 8 and copyBoard[row + 2][col + 1] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col + 1] + str(6 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos))  
+        if row + 2 < 8 and col - 1 >= 0 and copyBoard[row + 2][col - 1] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col - 1] + str(6 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos)) 
+        if row - 2 >= 0 and col - 1 >= 0 and copyBoard[row - 2][col - 1] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col - 1] + str(10 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos)) 
+        if row - 2 >= 0 and col + 1 < 8 and copyBoard[row - 2][col + 1] in "PQKNRB.":
+            fromPos = cols[col] + str(8 - row)
+            toPos = cols[col + 1] + str(10 - row)
+            moves.append(move(copy.deepcopy(board), fromPos, toPos)) 
     return moves
 
 def possibleMovesOpponent(board, row, col):
@@ -58,12 +119,12 @@ def possibleMovesOpponent(board, row, col):
     copyBoard = copy.deepcopy(board)
     moves = []
     if copyBoard[row][col] == 'P':
-        if copyBoard[row + 1][col] == '.' :
+        if copyBoard[row - 1][col] == '.' :
             fromPos = cols[col] + str(8 - row)
-            toPos = cols[col] + str(7 - row)
+            toPos = cols[col] + str(9 - row)
             moves.append(move(copy.deepcopy(board), fromPos, toPos))
-            if row == 1 and copyBoard[row + 1][col] == '.': 
-                toPos = cols[col] + str(6 - row)
+            if row == 6 and copyBoard[row - 2][col] == '.': 
+                toPos = cols[col] + str(10 - row)
                 moves.append(move(copy.deepcopy(board), fromPos, toPos))
     return moves
 
@@ -71,18 +132,31 @@ def respond(board):
     possibleMovesArray = []
     for row in range(0,8):
         for col in range(0,8):
-            if board[row][col] in "p":
-                possibleMovesArray.append(possibleMoves(board, row, col))
-
-    possibleMovesDepth_2 = []
-    bestMove = possibleMovesArray[0]
+            if board[row][col] in "pn":
+                possibleMoves(board, row, col, possibleMovesArray)
+    bestMinScore = -1000
+    respondMove = []
     for position in possibleMovesArray:
         for row in range(0,8):
             for col in range(0,8):
                 if position[row][col] in "P":
-                    possibleMovesDepth_2.append(possibleMovesOpponent(position, row, col))
-    return board
+                    possibleMovesDepth_2 = possibleMovesOpponent(position, row, col)
+                    minscore = 1000
+                    minmove = []
+                    for possibleBoards in possibleMovesDepth_2:
+                        score = scoreBoard(possibleBoards)
+                        printBoard(possibleBoards, -1)
+                        print(score)
+                        if score < minscore:
+                            minscore = score
+                            minmove = possibleBoards
+                    if bestMinScore < minscore:
+                        bestMinScore = minscore
+                        respondMove = position
+    return respondMove
 
+# what to do when equal scores
+# what to do regarding piece values
 
 
 a = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] 
@@ -102,18 +176,23 @@ gameEnded = False
 movenumber = 1
 while ~gameEnded :
     movefrom = input("Type your move from: ")
-    if movefrom == 'q':
-        exit()
     moveto = input("Type your move to: ")
-    board = move(board, movefrom, moveto)
-    printBoard(board, movenumber)
-    movenumber +=  1
+    if movefrom == 'q':
+        quit()
+    movefromvalid = (movefrom[0] in "abcdefgh") and (movefrom[1] in "12345678")
+    movetovalid = (moveto[0] in "abcdefgh") and (moveto[1] in "12345678")
+    if movefromvalid and movetovalid:
+        board = move(board, movefrom, moveto)
+        printBoard(board, movenumber)
+        movenumber +=  1
 
-    print("The Computer's Move . . .")
-    time.sleep(1)
+        print("The Computer's Move . . .")
+        time.sleep(1)
 
-    board = respond(board)
-    printBoard(board, movenumber)
-    movenumber +=  1
+        board = respond(board)
+        printBoard(board, movenumber)
+        movenumber +=  1
+    else :
+        print("INVALID MOVES try again")
 
 
